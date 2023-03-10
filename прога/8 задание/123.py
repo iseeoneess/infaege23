e39e4d5173bf49239db33a1e95395eb86c78910e
79533 <iseeone@mail.ru>
2023-03-10 23:31:56 +0300

    1982

--- прога/8 задание/123.py
+++ прога/8 задание/123.py
@@ -2,8 +2,8 @@ from itertools import product
 
 k = 0
 
-for i in product("ПУЛЯ", repeat = 6):
+for i in product("ЛОДКА", repeat = 4):
     s = "".join(i)
-    if s.count("У") == 2:
+    if s.count("О") >= 2:
         k += 1
         print(k, s)
