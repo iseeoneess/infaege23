d28397390060603bf48857b0cefc3ad605e50c7c
79533 <iseeone@mail.ru>
2023-03-19 00:00:15 +0300

    16(pizdec)

--- прога/8 задание/123.py
+++ прога/8 задание/123.py
@@ -1,8 +1,9 @@
-from itertools import product
+from itertools import permutations
+
 k = 0
 
-for x in product("ЖИРАФ", repeat = 5):
+for x in permutations("ПЕСКАРЬ"):
     slovo = ''.join(x)
-    if slovo.count("Ж") == 1 and (slovo[0] != 'Ф' and slovo[-1] != "Р"):
+    if slovo[0] != "Ь" and all(sub not in slovo for sub in ["ЬЕ", "ЬА", "ЬР"]):
         k += 1
         print(k, slovo)
