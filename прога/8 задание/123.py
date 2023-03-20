c2f908aa61caca1472ed4f3dca88ac7b2ba325d8
79533 <iseeone@mail.ru>
2023-03-21 02:26:39 +0300

    1935 код составил верно, но перепутал product с permutations... +-

--- прога/8 задание/123.py
+++ прога/8 задание/123.py
@@ -1,9 +1,9 @@
-from itertools import permutations
+from itertools import permutations, product
 
 k = 0
 
-for x in permutations("ДЕЙКСТРА", r = 6):
+for x in permutations("ВАЙФУ", r = 4):
     slovo = "".join(x)
-    if any(sub in slovo for sub in ['ЙД','ЙК','ЙС','ЙТ','ЙР']):
+    if slovo [0] != "Й" and "ВФ" not in slovo and "ФВ" not in slovo:
         k += 1
         print(k, slovo)
