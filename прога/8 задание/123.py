5bf6f198cdb71fe1a11b445ff0233961c796be1c
79533 <iseeone@mail.ru>
2023-03-21 01:36:08 +0300

    947  1) Допустил ошибку, ведь одинаковые элементы тоже учитываются. 2) В алфавитном порядке значит то, что символы первые символ должен быть меньше второго и так далее.

--- прога/8 задание/123.py
+++ прога/8 задание/123.py
@@ -1,8 +1,8 @@
 from itertools import product
 
 k = 0
-for x in product("ВИШНЯ", repeat = 6):
+for x in product("ABCD", repeat = 4):
     slovo = "".join(x)
-    if slovo.count("В") <= 1 and slovo[0] != "Ш" and slovo[-1] not in "ИЯ":
+    if slovo[0] <= slovo [1] <= slovo[2] <= slovo[3]:
         k += 1
         print(k, slovo)
