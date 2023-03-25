d657ade564063c85fa6c3aff82bb6fc6509836db
79533 <iseeone@mail.ru>
2023-03-25 23:31:04 +0300

    988

--- прога/8 задание/123.py
+++ прога/8 задание/123.py
@@ -2,8 +2,9 @@ from itertools import permutations, product
 
 k = 0
 
-for x in product("АГИЛМОРТ", repeat = 4):
+for x in product("АИМРЯ", repeat = 4):
     slovo = ''.join(x)
     k += 1
-    if slovo[-2] == "И" and slovo[-1] == "М":
+    if slovo == "АРИЯ":
         print(k, slovo)
+        break
