231ae0b73419c3a09a85001e95d1d858c322e527
79533 <iseeone@mail.ru>
2023-03-27 03:07:08 +0300

    289

--- testing.py
+++ testing.py
@@ -1,1 +1,5 @@
-aboba
+lst = ["1", '2', '3']
+d = ""
+for i in range(len(lst)):
+    d = d + "".join(lst[i])
+print(d)
--- прога/14 задание/14.py
+++ прога/14 задание/14.py
@@ -1,6 +1,17 @@
-x = 2*27**7 + 3**10 - 9
-lst = []
-while x > 0:
-   lst = [x % 3] + lst
-   x = x // 3
-print(lst.count(0))
+for x in range(2, 1000):
+    num = 81 ** 20 - 9 ** x + 50
+    lst = []
+    d = ""
+    while num > 0:
+        lst = [num % 9] + lst
+        num = num // 9
+
+    # for i in range(len(lst)):
+    #     lst[i] = str(lst[i])
+    #
+    # for g in range(len(lst)):
+    #     d = d + "".join(lst[g])
+
+    if sum(lst) == 138:
+        print(x, sum(lst))
+        break
