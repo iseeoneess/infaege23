b56f5f43df62beeed556ef85e7982adc3287e9dd
79533 <iseeone@mail.ru>
2023-03-27 18:07:11 +0300

    алгоритм для перевода числа в 10 сс.

--- прога/14 задание/14.py
+++ прога/14 задание/14.py
@@ -1,6 +1,11 @@
-for x in range(1, 100):
-    num1 = 5 + 15*x + 3*15**2 + 2*15**3 + 15**4
-    num2 = 3 + 3*15 + 2*15**2 + x*15**3 + 15**4
-    if (num1 + num2) % 14 == 0:
-        print(x, (num1 + num2) // 14)
+num = 3364
 
+cc = 12
+
+num = str(num)[::-1]
+lst = []
+
+for i in range(len(num)):
+    lst = [int(num[i]) * cc**i] + lst
+
+print(sum(lst))
--- прога/8 задание/123.py
+++
@@ -1,4 +0,0 @@
-for x in "0123456789abcdefg":
-    num = int(f"9759{x}", 17) + int(f"3{x}108", 17)
-    if num % 11 == 0:
-        print(x, num // 11)
