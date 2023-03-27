c2e64a07c368f6fc3cfe6e8e455a1364490a17a7
79533 <iseeone@mail.ru>
2023-03-27 04:22:58 +0300

    183

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 for n in range(2, 101):
-    num1 = 3 + 2*n + n**2
-    num2 = 3 + 9*(n+2)
-    if num1 == num2:
+    num1 = 4 + 5*n + n**2
+    num2 = int("35", 9)
+    num3 = 7*(n + 1) + pow(n+1, 2)
+    if num1 + num2 == num3:
         print(n)
-        break
-# proverka, esli 2 <= n <= 32.
-print(int("123", 9), int("93", 11))
+# proverka, esli 2 <= n <= 32
+print(int("154", 7) + int("35", 9), int("170", 8))
