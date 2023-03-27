8c574dbcb4c4d31d2c066072ab78194acf26ebc9
79533 <iseeone@mail.ru>
2023-03-27 03:12:01 +0300

    290

--- прога/14 задание/14.py
+++ прога/14 задание/14.py
@@ -1,17 +1,10 @@
 for x in range(2, 1000):
-    num = 81 ** 20 - 9 ** x + 50
+    num = 64**12 - 8**14 + x
     lst = []
     d = ""
     while num > 0:
-        lst = [num % 9] + lst
-        num = num // 9
+        lst = [num % 8] + lst
+        num = num // 8
 
-    # for i in range(len(lst)):
-    #     lst[i] = str(lst[i])
-    #
-    # for g in range(len(lst)):
-    #     d = d + "".join(lst[g])
-
-    if sum(lst) == 138:
-        print(x, sum(lst))
-        break
+    if lst.count(7) == 12 and lst.count(1) == 1:
+        print(x)
