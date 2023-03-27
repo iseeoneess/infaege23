322f6f92b8efdf91fbfa1c474b13079c3ea13a6f
79533 <iseeone@mail.ru>
2023-03-27 04:17:31 +0300

    183

--- testing.py
+++ testing.py
@@ -1,5 +1,8 @@
-lst = ["1", '2', '3']
-d = ""
-for i in range(len(lst)):
-    d = d + "".join(lst[i])
-print(d)
+for n in range(2, 101):
+    num1 = 3 + 2*n + n**2
+    num2 = 3 + 9*(n+2)
+    if num1 == num2:
+        print(n)
+        break
+# proverka, esli 2 <= n <= 32.
+print(int("123", 9), int("93", 11))
