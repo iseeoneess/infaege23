d427fe5f472adef170ea03be222c86b833b60dd4
79533 <iseeone@mail.ru>
2023-03-27 15:52:23 +0300

    4702 xy for

--- testing.py
+++ testing.py
@@ -1,11 +1,14 @@
-for n in range(1, 10000):
+for x in range(1, 10000):
+    k = 0
     def ssWhile(x, c):
         lst = []
         while x > 0:
             lst = [x % c] + lst
             x //= c
         return lst
-    frNum = ssWhile(n, 7)
-    scNum = ssWhile(n, 6)
-    thrNum = ssWhile(n, 13)
-    if len(frNum) == 2 and len(scNum) == 3 and thrNum[-1] == 3: print(n)
+    frNum = ssWhile(x, 5)
+    scNum = ssWhile(x, 2)
+    thrNum = str(ssWhile(x, 16))
+    if len(frNum) <= 4 and len(scNum) >= 5 and thrNum[-1:-3] == "12":
+        k += 1
+        print(k, x)
--- прога/14 задание/14.py
+++ прога/14 задание/14.py
@@ -1,3 +1,6 @@
-for x in range(2, 41):
-    if x % int("10000", 2) == int("1011", 2):
-        print(x)
+for x in range(1, 100):
+    num1 = 5 + 15*x + 3*15**2 + 2*15**3 + 15**4
+    num2 = 3 + 3*15 + 2*15**2 + x*15**3 + 15**4
+    if (num1 + num2) % 14 == 0:
+        print(x, (num1 + num2) // 14)
+
