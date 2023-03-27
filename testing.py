3df338970e6c41b1a800b3f55c5c415c719ab0a0
79533 <iseeone@mail.ru>
2023-03-27 15:31:44 +0300

    257 (analog)

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ for n in range(1, 10000):
             lst = [x % c] + lst
             x //= c
         return lst
-    frNum = ssWhile(n, 6)
-    scNum = ssWhile(n, 5)
-    thrNum = ssWhile(n, 11)
-    if len(frNum) == 2 and len(scNum) == 3 and thrNum[-1] == 1: print(n)
+    frNum = ssWhile(n, 7)
+    scNum = ssWhile(n, 6)
+    thrNum = ssWhile(n, 13)
+    if len(frNum) == 2 and len(scNum) == 3 and thrNum[-1] == 3: print(n)
