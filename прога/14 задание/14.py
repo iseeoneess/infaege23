d8b762d57012a2d63ecf2303b7c1197521e63c35
79533 <iseeone@mail.ru>
2023-03-27 12:56:26 +0300

    243

--- прога/14 задание/14.py
+++ прога/14 задание/14.py
@@ -1,5 +1,38 @@
-for x in "123456789abcdefghijkl":
-    for y in "0123456789abc":
-        num = int(f"{x}23{x}5", 22) - int(f"67{y}9{y}", 13)
-        if num % 57 == 0:
-            print(x, y, int(x, 22) + int(y, 13) // 57, num // 57)
+for n in range(2, 1000):
+    num = 2 + 3*n + n**2
+    num2 = 11
+    equal = 4 + 2*(n+1) + pow((n + 1), 2)
+    if num + num2 == equal:
+        print(n)
+        break
+# proverka:
+print(int("132", 6), int("13", 8), int("123", 7))
+
+# for n in range(2, 100001):
+#     try:
+#         num1 = 132
+#         num2 = 13
+#         num3 = 124
+#
+#
+#         def lstStr(num, n):
+#             d = ""
+#             lst = []
+#             while num > 0:
+#                 lst = [num % n] + lst
+#                 num //= n
+#             for i in range(len(lst)):
+#                 lst[i] = str(lst[i])
+#             for g in range(len(lst)):
+#                 d = d + "".join(lst[g])
+#             return d
+#
+#
+#         res1 = int(lstStr(num1, n), n)
+#         res2 = int(lstStr(num2, 8), 8)
+#         res3 = int(lstStr(num3, n + 1), n + 1)
+#         if res1 + res2 == res3:
+#             print(n)
+#             break
+#     except:
+#         pass
