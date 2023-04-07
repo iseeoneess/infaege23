389abad237abf2fff0ee05acd106dace2c6153f7
79533 <iseeone@mail.ru>
2023-04-07 22:06:56 +0300

    N# 1422 (2)

--- прога/2 задание/2.py
+++ прога/2 задание/2.py
@@ -2,12 +2,12 @@ from itertools import *
 
 
 def f(x, y, z, w):
-    return ((w <= y) or (not (y <= z))) and (not x) and (not (x == z))
+    return (not (z and (not w))) or ((z <= w) == (x <= y))
 
 
-for a in product([0, 1], repeat=5):
-    table = [(0, a[0], 1, a[1]), (1, a[2], a[3], 1), (0, a[4], 1, 1)]
+for a in product([0, 1], repeat=6):
+    table = [(1, a[0], a[1], a[2]), (1,1,a[3],1), (1, a[4], a[5], 1)]
     if len(table) == len(set(table)):
         for p in permutations("xyzw"):
-            if [f(**dict(zip(p, row))) for row in table] == [1,1,1]:
+            if [f(**dict(zip(p, row))) for row in table] == [0,0,0]:
                 print(*p, sep = "")
