ca5894b5074aa5607642f487702db04a148825f0
79533 <iseeone@mail.ru>
2023-04-08 15:43:58 +0300

    432

--- прога/15 задание/15.py
+++ прога/15 задание/15.py
@@ -1,5 +1,4 @@
-def f(x):
-    return ((x % a == 0) and (x % 24 == 0) and (x % 16 != 0)) <= (x % a != 0)
+def f(x): return ((x % 84 != 0) or (x % 90 != 0)) <= (x % a != 0)
 
-for a in range(1, 100):
-    if all(f(x) for x in range(1, 100000)): print(a)
+for a in range(1, 10000):
+    if all(f(x) for x in range(10000)): print(a)
