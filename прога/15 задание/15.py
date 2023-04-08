edc43fc2409f98e421572308390baa1ce39d4a7d
79533 <iseeone@mail.ru>
2023-04-08 15:54:32 +0300

    1127

--- прога/15 задание/15.py
+++ прога/15 задание/15.py
@@ -1,4 +1,6 @@
-def f(x): return (((x % 4) != 3) or ((x % 6) != 1)) <= ((x % 36) != a)
+def f(x): return (a % 7 == 0) and ((240 % x == 0) <= ((a % x != 0) <= (780 % x != 0)))
 
 for a in range(1, 100000):
-    if all(f(x) for x in range(1000000)): print(a)
+    if all(f(x) for x in range(1, 1000000)):
+        print(a)
+        break
