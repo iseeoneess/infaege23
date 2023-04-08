a1b6ccbffada50b2d5af51807bc97066d2bca90a
79533 <iseeone@mail.ru>
2023-04-08 19:51:53 +0300

    743 код верный, но не прочитал задание (+-)

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
-def f(x, y, a): return (x * y > a) and (x > y) and (x < 8)
+def f(x,y,a): return (2*x + y != 70) or (x < y) or (a < x)
 
-for a in range(100):
-    if all(f(x,y,a) == 0 for x in range(1000) for y in range(1000)):
-        print(a)
-        break
+lst = []
+for a in range(1000):
+    if all(f(x,y,a) for x in range(1000) for y in range(1000)):
+        lst += [a]
+
+print(sorted(lst)[-1])
