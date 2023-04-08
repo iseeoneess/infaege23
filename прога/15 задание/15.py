cd27cb76b3e62338d90b0af380834677120c0ee1
79533 <iseeone@mail.ru>
2023-04-08 16:13:47 +0300

    2078

--- прога/15 задание/15.py
+++ прога/15 задание/15.py
@@ -1,6 +1,6 @@
-def f(x): return (a % 7 == 0) and ((240 % x == 0) <= ((a % x != 0) <= (780 % x != 0)))
+def f(x): return (x & 107 == 0) <= ((x & 55 != 0) <= (x & a != 0))
 
-for a in range(1, 100000):
-    if all(f(x) for x in range(1, 1000000)):
+for a in range(1, 1000000):
+    if all(f(x) for x in range(1,1000000)):
         print(a)
         break
