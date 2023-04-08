2c47a9b36d60b55e0d52616b1a54d1323a8c8a58
79533 <iseeone@mail.ru>
2023-04-08 20:01:22 +0300

    1409

--- прога/15 задание/15.py
+++ прога/15 задание/15.py
@@ -1,8 +1,17 @@
-def f(x,y,a): return (2*x + y != 70) or (x < y) or (a < x)
+a = set()
+p = set(range(2,21,2))
+q = set(range(3,31,3))
+r = set(range(12,61,12))
 
-lst = []
-for a in range(1000):
-    if all(f(x,y,a) for x in range(1000) for y in range(1000)):
-        lst += [a]
+def f(x):
+    A = x in a
+    P = x in p
+    Q = x in q
+    R = x in r
+    return (not A) <= ((P and Q) <= R)
 
-print(sorted(lst)[-1])
+for x in range(10000):
+    if f(x) == 0:
+        a.add(x)
+
+print(a)
