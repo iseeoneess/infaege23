d5a97b4529b6e0a36d6e23a9781733fbfa0ecd54
79533 <iseeone@mail.ru>
2023-06-07 23:50:54 +0300

    4871(5)

--- прога/5 задание/5.py
+++ прога/5 задание/5.py
@@ -1,11 +1,14 @@
 lst_res = []
 
-for n in range(1, 100001):
-    bn = str(n)
-    chetnSum = sum(int(x) for x in bn if int(x) % 2 == 0)
-    if len(bn) == 1: chet_indexNum = 0
-    else: chet_indexNum = sum(int(bn[z]) for z in range(len(bn)) if z % 2 != 0)
-    res = abs(chetnSum - chet_indexNum)
-    if res == 9: lst_res.append(n)
 
-print(min(lst_res))
+for n in range(2, 10000001):
+    bn = n
+    if bn % 3 == 0: bn = int(bn / 3)
+    else: bn -= 1
+    if bn % 5 == 0: bn = int(bn / 5)
+    else: bn -= 1
+    if bn % 11 == 0: bn = int(bn / 11)
+    else: bn -= 1
+    if bn == 8: lst_res.append(n)
+
+print(len(lst_res))
