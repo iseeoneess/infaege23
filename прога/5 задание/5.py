40abe74ba1746abaf2c43b41ee661959889b2519
79533 <iseeone@mail.ru>
2023-06-08 00:29:09 +0300

    1732(5)

--- прога/5 задание/5.py
+++ прога/5 задание/5.py
@@ -1,25 +1,9 @@
-from math import *
-
-lst_res = []
-
-for m in range(1, 1001):
-    n = 120
-    bn = str(n)
-    bm = str(m)
-    bn_chet = [int(x) for x in bn if (int(x) % 2 == 0 and x != '0')]
-    bn_nechet = [int(x) for x in bn if int(x) % 2 != 0]
-
-    bm_chet = [int(x) for x in bm if (int(x) % 2 == 0 and x != '0')]
-    bm_neChet = [int(x) for x in bm if int(x) % 2 != 0]
-
-    p_chet = bn_chet + bm_chet
-    p_nechet = bn_nechet + bm_neChet
-
-    p_prodChet = prod(p_chet)
-    p_prodNeChet = prod(p_nechet)
-
-    res = abs(p_prodNeChet - p_prodChet)
-    if res == 29: lst_res.append(m)
-
-
-print(lst_res)
+for n in range(1,10001):
+    bn = bin(2*n)[2:]
+    for i in range(2):
+        if bn.count('1') % 2 == 0: bn += '0'
+        else: bn += '1'
+    r = int(bn,2)
+    if r >1017:
+        print(n, r)
+        break
