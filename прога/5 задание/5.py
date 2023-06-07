60ecb57c05837d41289baf21057ee7cade2a7adb
79533 <iseeone@mail.ru>
2023-06-07 23:42:09 +0300

    4867(5)

--- прога/5 задание/5.py
+++ прога/5 задание/5.py
@@ -1,14 +1,11 @@
-from itertools import *
+lst_res = []
 
-lst = []
-for n in range(300, 401):
-    lst_nums = [] # буферные списки только внутри цикла, иначе переполниться стэк!
-    for x in permutations(f'{n}', r = 2):
-        s = ''.join(x)
-        if s[0] != '0':
-            lst_nums.append(int(s))
-    res = max(lst_nums) - min(lst_nums)
+for n in range(1, 100001):
+    bn = str(n)
+    chetnSum = sum(int(x) for x in bn if int(x) % 2 == 0)
+    if len(bn) == 1: chet_indexNum = 0
+    else: chet_indexNum = sum(int(bn[z]) for z in range(len(bn)) if z % 2 != 0)
+    res = abs(chetnSum - chet_indexNum)
+    if res == 9: lst_res.append(n)
 
-    if res == 20:
-        lst.append(n)
-print(len(lst))
+print(min(lst_res))
