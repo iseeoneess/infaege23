f30ee6b62c8552642ce3c3b6a3647a3f87ba4df1
79533 <iseeone@mail.ru>
2023-06-07 21:50:28 +0300

    2461(5)

--- прога/5 задание/5.py
+++ прога/5 задание/5.py
@@ -1,10 +1,10 @@
 lst = []
 
-for n in range(1000, 10000):
+for n in range(100, 1000):
     bn = str(n)
-    p_1 = str(int(bn[0]) * int(bn[1]))
-    p_2 = str(int(bn[2]) * int(bn[3]))
-    res = int(str(min(int(p_1), int(p_2))) + str(max(int(p_1), int(p_2))))
-    if res == 1214: lst.append(n)
-
+    firstEq = int(bn[0])**2 + int(bn[1])**2
+    secondEq = int(bn[1])**2 + int(bn[2])**2
+    res = str(max(firstEq,secondEq)) + str(min(firstEq,secondEq))
+    if res == '9010':
+        lst.append(n)
 print(lst)
