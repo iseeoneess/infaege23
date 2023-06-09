67a72d958ce7cc76dc4d4d41d17d2051c4ee55a8
79533 <iseeone@mail.ru>
2023-06-09 18:24:40 +0300

    2237(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,13 +1,12 @@
-from functools import *
-
 lst_res = []
-@lru_cache(None)
+
+
 def f(n):
-    if n > 30: return n ** 2 + 5 * n + 4
-    if n <= 30 and n % 2 == 0: return f(n + 1) + 3 * f(n + 4)
-    if n <= 30 and n % 2 != 0: return 2 * f(n + 2) + f(n + 5)
+    if n == 0: return 0
+    if n > 0 and n % 2 == 0: return f(n / 2)
+    if n % 2 != 0: return 1 + f(n - 1)
 
-for n in range(1, 1001):
-    if sum(int(x) for x in str(f(n))) == 27: lst_res.append(n)
 
+for n in range(1, 501):
+    if f(n) == 8: lst_res.append(n)
 print(len(lst_res))
