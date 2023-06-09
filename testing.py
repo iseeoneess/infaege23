e6a854f5d058e4a1ef8570ba7a9e640daa7ceb97
79533 <iseeone@mail.ru>
2023-06-09 17:37:33 +0300

    2223(12)

--- testing.py
+++ testing.py
@@ -1,6 +1,10 @@
-for a in range(-100, 101):
-    for b in range(-100, 101):
-        x = -5 + 4*(5 + a) + 90
-        y = 15 + 4*(1 + b) + 4
-        if x == 5 and y == 3:
-            print(a, b)
+a = 'КАРА'
+k = 1
+b = 'Т'
+i = len(a)
+while i > 1:
+    c = a[i - 1]
+    b = b + c
+    i -= k
+
+print(b)
