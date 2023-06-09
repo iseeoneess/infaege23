8eaf7adfa11aef6960731e4b1cc3dc38b4b1010f
79533 <iseeone@mail.ru>
2023-06-09 18:15:02 +0300

    628(16) Внимательно читать условие!

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,13 +1,15 @@
-from functools import lru_cache
+from functools import *
 
+lst_res = []
 @lru_cache(None)
 def f(n):
-    if n == 1: return 1
-    if n > 1: return f(n - 1) - 2 * g(n - 1)
+    if n <= 18: return n + 3
+    if n > 18 and n % 3 == 0: return (n // 3) * f(n // 3) + n - 12
+    if n > 18 and n % 3 != 0: return f(n - 1) + n ** 2 + 5
 
-@lru_cache(None)
-def g(n):
-    if n == 1: return 1
-    if n > 1: return f(n-1) + g(n-1) + n
-k = str(g(36))
-print(sum(int(x) for x in k))
+for n in range(1, 1001):
+    k = str(f(n))
+    if all(int(p) % 2 == 0 for p in k): lst_res.append(n)
+    else:continue
+
+print(len(lst_res))
