e59590b20fb698877af7062983159bb783269b9c
79533 <iseeone@mail.ru>
2023-06-09 19:30:28 +0300

    2248(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,18 +1,13 @@
 from functools import *
 
 
-k = 0
 @lru_cache(None)
 def f(n):
-    global k
-    k += 1
-    if n >= 1:
-        k += 1
-        f(n - 1)
-        f(n - 3)
-        k += 1
+    if n <= 1: return n
+    if n > 1 and n % 3 == 0: return n + f(n / 3)
+    if n > 1 and n % 3 != 0: return n + f(n + 3)
 
-f(40)
-print(k)
-
-
+for n in range(1, 1001):
+    try:
+        if f(n) > 100: print(n)
+    except: pass
