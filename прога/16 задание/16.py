e95827cf809784be5291210991ecefde6e636b41
79533 <iseeone@mail.ru>
2023-06-09 18:26:48 +0300

    1131(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -2,11 +2,10 @@ lst_res = []
 
 
 def f(n):
-    if n == 0: return 0
-    if n > 0 and n % 2 == 0: return f(n / 2)
-    if n % 2 != 0: return 1 + f(n - 1)
+    if n == 1: return 1
+    if n >= 2 and n % 2 == 0: return f(n / 2) + 1
+    if n >= 2 and n % 2 != 0: return f(n - 1) + n
 
 
-for n in range(1, 501):
-    if f(n) == 8: lst_res.append(n)
-print(len(lst_res))
+for n in range(1, 1001):
+    if f(n) == 19: print(n)
