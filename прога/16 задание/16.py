8a02a6eb1da4eaf01f673814a1aec18336ec7daa
79533 <iseeone@mail.ru>
2023-06-09 17:59:49 +0300

    1860(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,8 +1,7 @@
 def f(n):
-    if n == 1: return 1
-    if n == 2: return 2
-    if n > 2 and n % 2 == 0: return int((n + f(n - 2)) / 5)
-    if n > 2 and n % 2 != 0: return int((2 * n + f(n - 1) + f(n - 2)) / 4)
+    if n <= 1: return 0
+    if n > 1 and n % 2 != 0: return f(n - 1) + 3 * n ** 2
+    if n > 1 and n % 2 == 0: return n/2 + f(n-1) + 2
 
 
-print(f(50))
+print(f(49))
