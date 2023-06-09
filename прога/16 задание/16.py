9d436e600f543a0d007b89538c0330f7f2a54219
79533 <iseeone@mail.ru>
2023-06-09 17:50:40 +0300

    608(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,7 +1,5 @@
 def f(n):
-    if n <= 10: return n
-    if 10 < n <= 36: return n // 4 + f(n - 10)
-    if n > 36: return 2 * f(n - 5)
+    if n == 1: return 2
+    if n > 1: return f(n -1) + 5*n**2
 
-
-print(f(100))
+print(f(39))
