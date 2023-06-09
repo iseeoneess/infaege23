f805343575cc1581a2edec1d6599e643ec588754
79533 <iseeone@mail.ru>
2023-06-09 18:01:50 +0300

    592(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,7 +1,9 @@
 def f(n):
-    if n <= 1: return 0
-    if n > 1 and n % 2 != 0: return f(n - 1) + 3 * n ** 2
-    if n > 1 and n % 2 == 0: return n/2 + f(n-1) + 2
+    if n == 1: return 1
+    if n > 1: return f(n - 1) - n * g(n - 1)
 
 
-print(f(49))
+def g(n):
+    if n == 1: return 1
+    if n > 1: return f(n - 1) + 2 * g(n - 1)
+print(g(18))
