54265129f9578edd4ea593d9b2307e756521a529
79533 <iseeone@mail.ru>
2023-06-09 18:05:06 +0300

    593(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,9 +1,13 @@
+from functools import lru_cache
+
+@lru_cache(None)
 def f(n):
     if n == 1: return 1
-    if n > 1: return f(n - 1) - n * g(n - 1)
+    if n > 1: return f(n - 1) - 2 * g(n - 1)
 
-
+@lru_cache(None)
 def g(n):
     if n == 1: return 1
-    if n > 1: return f(n - 1) + 2 * g(n - 1)
-print(g(18))
+    if n > 1: return f(n-1) + g(n-1) + n
+k = str(g(36))
+print(sum(int(x) for x in k))
