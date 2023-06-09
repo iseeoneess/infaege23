b3ce80fc16c62aafa6471fc1a079f86e1e889a83
79533 <iseeone@mail.ru>
2023-06-09 17:48:33 +0300

    596(16)

--- прога/16 задание/16.py
+++ прога/16 задание/16.py
@@ -1,7 +1,8 @@
 def f(n):
-    if n == 0: return 1
-    if n == 1: return 3
-    if n == 2: return 2
-    if n > 2: return f(n - 1) * f(n - 3)
+    if n <= 3: return n
+    if n > 3 and n % 3 == 0: return n ** 3 + f(n - 1)
+    if n > 3 and n % 3 == 1: return 4 + f(n // 3)
+    if n > 3 and n % 3 == 2: return n ** 2 + f(n - 2)
 
-print(f(7))
+
+print(f(100))
