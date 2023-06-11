44bc2b1bac80ba2e45eb6a150afe8f9ebc3de4dc
79533 <iseeone@mail.ru>
2023-06-11 17:26:53 +0300

    2342(23)

--- прога/23 задание/23.py
+++ прога/23 задание/23.py
@@ -1,7 +1,9 @@
 def f(c, e):
     if c > e: return 0
     if c == e: return 1
-    if c < e: return f(c + 1, e) + f(c * 2, e) + f(c * 2 + 1, e)
+    if c < e:
+        if int(str(c)[0]) < 9 and int(str(c)[1]) < 9: return f(c + 1, e) + f(c + 11, e)
+        if int(str(c)[0]) == 9 and int(str(c)[1]) < 9: return f(c + 1, e) + f(c + 1, e)
+        if int(str(c)[0]) < 9 and int(str(c)[1]) == 9: return f(c + 1, e) + f(c + 10, e)
 
-
-print(f(4, 29))
+print(f(25, 51))
