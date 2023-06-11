9dae9ee4e17331cb8701544a0d4b04c262d0ba56
79533 <iseeone@mail.ru>
2023-06-11 16:40:38 +0300

    951(23)

--- прога/23 задание/23.py
+++ прога/23 задание/23.py
@@ -1,7 +1,7 @@
 def f(c, e):
-    if c > e or c == 6: return 0
+    if c > e or c == 11 or c == 18: return 0
     if c == e: return 1
-    if c < e: return f(c + 2, e) + f(c * 3, e)
+    if c < e: return f(c + 1, e) + f(c + 2, e) + f(c * 3, e)
 
 
-print(f(1, 25) * f(25, 63))
+print(f(4, 8) * f(8, 23))
