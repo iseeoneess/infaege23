ad6934cb59300b8f93aa75a429d96aeb7454ca93
79533 <iseeone@mail.ru>
2023-06-11 21:36:16 +0300

    4275(23)

--- прога/23 задание/23.py
+++ прога/23 задание/23.py
@@ -4,9 +4,7 @@ from functools import *
 @lru_cache(None)
 def f(c, e, k):
     if c > e: return 0
-    if c == e and k == 1: return 1
-    if c == e and k != 1: return 0
-    if c < e: return f(c + 1, e, k) + f(c + 2, e, k) + f(c * 2, e, k + 1)
-
-
-print(f(2, 12, 0))
+    if c == e and k <= 3: return 1
+    if c == e and k > 3: return 0
+    return f(c + 2, e, k) + f(c * 3, e, k + 1) + f(c * 5, e, k + 1)
+print(f(2,200,0))
