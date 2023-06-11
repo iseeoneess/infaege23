f9cd0d5a47323ac442d2cd7621bcf117ca96fc0b
79533 <iseeone@mail.ru>
2023-06-11 16:35:44 +0300

    1376(23)

--- прога/23 задание/23.py
+++ прога/23 задание/23.py
@@ -1,7 +1,6 @@
 def f(c, e):
-    if c > e: return 0
+    if c < e: return 0
     if c == e: return 1
-    if c < e: return f(c + 1, e) + f(c + 3, e) + f(c * 2, e)
+    if c > e: return f(c - 8, e) + f(c // 2, e)
 
-
-print(f(3, 9) * f(9, 12) * f(12, 20))
+print(f(102, 43) * (f(43, 5)))
