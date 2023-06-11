2be9ce2a935ca7e5c29db8bb9f47923e2266a1ab
79533 <iseeone@mail.ru>
2023-06-11 21:20:48 +0300

    2341(23)

--- прога/23 задание/23.py
+++ прога/23 задание/23.py
@@ -2,13 +2,17 @@ from functools import *
 
 d = set()
 
+
 @lru_cache(None)
-def f(c, k):
-    if k == 15:
-        d.add(c)
-    if k != 15:
-        f(c * 2, k + 1)
-        f(c * 2 + 1, k + 1)
+def f(c,k):
+    if k == 8:
+        if 1000 <= c <= 1024:
+            d.add(c)
+    else:
+        f(c + 1, k + 1)
+        f(c + 5, k + 1)
+        f(c * 3, k + 1)
 
 f(1,0)
+
 print(len(d))
