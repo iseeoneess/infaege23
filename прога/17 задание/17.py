5996d34b09918e6d3f1ac224fb6bd1ebd56a5177
79533 <iseeone@mail.ru>
2023-06-15 00:06:42 +0300

    2015(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,9 +1,9 @@
-a = [int(x) for x in open('17_2013.txt')]
+a = [int(x) for x in open('17_2015.txt')]
 
 ans = []
 
 for i in range(len(a)):
-    if any(str(a[i])[-1] == z for z in ['2','7']) and all(a[i] % z == 0 for z in [3,11]):
+    if any(str(a[i])[-1] == z for z in ['5','7']) and all(a[i] % z != 0 for z in [9,11]):
         ans.append(a[i])
 
-print(len(ans), min(ans))
+print(len(ans), min(ans) + max(ans))
