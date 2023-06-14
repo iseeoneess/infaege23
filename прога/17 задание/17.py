dcf5dd62a258f91a0c02f7372360061515c76340
79533 <iseeone@mail.ru>
2023-06-15 00:27:41 +0300

    1993(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,12 +1,9 @@
-a = [int(x) for x in open('17_2017.txt')]
-
+a = [int(x) for x in open('17_1993.txt')]
 
 ans = []
 
+for i in range(len(a)-1):
+    if (a[i] + a[i + 1]) % 3 == 0 and (a[i] + a[i + 1]) % 6 != 0 and str(a[i] * a[i + 1])[-1] == '8':
+        ans.append(a[i] + a[i + 1])
 
-
-for i in range(len(a)):
-    if (a[i] % 16) == 11 and a[i] % 7 == 0 and all(a[i] % z != 0 for z in [6,13,19]):
-        ans.append(a[i])
-
-print(sum(x for x in ans), len(ans))
+print(len(ans), max(ans))
