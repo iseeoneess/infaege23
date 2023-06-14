f024b46504643b25bdcb272cbb44fd030ff04c7f
79533 <iseeone@mail.ru>
2023-06-14 20:13:34 +0300

    843(19-21)

--- прога/19-21 задание/19-21.py
+++ прога/19-21 задание/19-21.py
@@ -1,11 +1,11 @@
 def f(a, b, m):
-    if a * b >= 63: return m % 2 == 0
+    if a + b <= 20: return m % 2 == 0
     if m == 0: return 0
-    h = [f(a + 1, b, m - 1), f(a * 2, b, m - 1),
-         f(a, b + 1, m - 1), f(a, b * 2, m - 1)]
+    h = [f(a - 1, b, m - 1), f(a // 2 + 1 if a % 2 != 0 else a // 2, b, m - 1),
+         f(a, b - 1, m - 1), f(a, b // 2 + 1 if b % 2 != 0 else b // 2, m - 1)]
     return any(h) if (m - 1) % 2 == 0 else all(h)
 
-print('19)', *[s for s in range(1, 32) if not f(2, s, 1) and f(2, s, 2)])
 
-print('20)', *[s for s in range(1, 32) if not f(2, s, 1) and f(2, s, 3)])
-print('21)', *[s for s in range(1, 32) if not f(2, s, 2) and f(2, s, 4)])
+print('19)', [s for s in range(11, 1001) if f(10, s, 2)])
+print('20)', [s for s in range(11, 1001) if not f(10, s, 1) and f(10, s, 3)])
+print('21)', [s for s in range(11, 1001) if not f(10, s, 2) and f(10, s, 4)])
