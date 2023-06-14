de1b1a8fc2fe1ef1699901e10662780c731c2c9a
79533 <iseeone@mail.ru>
2023-06-15 00:37:18 +0300

    1998(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,12 +1,9 @@
-from math import *
+a = [int(x) for x in open('17_1998.txt')]
 
-a = [int(x) for x in open('17_1994.txt')]
+ans = []
 
-ans1 = []
-ans2 =[]
-for i in range(len(a) - 1):
-   if (a[i] * a[i + 1]) > 0 and (a[i] + a[i + 1]) % 7 == 0:
-       ans2.append(a[i]*a[i + 1])
+for i in range(len(a) - 2):
+    if (a[i] * a[i + 1] * a[i + 2]) % 7 == 0 and str(a[i] + a[i + 1] + a[i + 2])[-1] == '5':
+        ans.append(a[i] + a[i + 1] + a[i + 2])
 
-print(len(ans1))
-print(min(ans2))
+print(len(ans), max(ans))
