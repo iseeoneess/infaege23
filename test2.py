5c08846327b99c0da079cb170de657b96c6f5b5d
79533 <iseeone@mail.ru>
2023-06-15 00:22:42 +0300

    2017(17)

--- test2.py
+++ test2.py
@@ -1,6 +1,8 @@
-def f(c, e, f6):
-    if c == 6: f6 = 1
-    if c < e: return 0
-    if c == e: return f6 == 1
-    return f(c - 2, e, f6) + f(c // 3, e,f6)
-print(f(200,2, 0))
+def f(n, cc):
+    lst = []
+    while n > 0:
+        lst = [n % cc] + lst
+        n //= cc
+    return ''.join([str(x) for x in lst])
+
+print(f(1035,16))
--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,9 +1,12 @@
-a = [int(x) for x in open('17_2016.txt')]
+a = [int(x) for x in open('17_2017.txt')]
+
 
 ans = []
 
+
+
 for i in range(len(a)):
-    if a[i] % 13 == 7 and all(a[i] % z != 0 for z in [7,11]):
+    if (a[i] % 16) == 11 and a[i] % 7 == 0 and all(a[i] % z != 0 for z in [6,13,19]):
         ans.append(a[i])
 
-print(max(ans) - min(ans), len(ans))
+print(sum(x for x in ans), len(ans))
