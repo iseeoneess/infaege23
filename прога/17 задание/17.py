2ce7a64e811e8c030f2949e8958b2610eb51e189
79533 <iseeone@mail.ru>
2023-06-15 19:51:07 +0300

    2402(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,9 +1,15 @@
-a = [int(x) for x in open('17_1999.txt')]
+a = [int(x) for x in open('17_2402.txt')]
 
 ans = []
+def f(n, cc):
+    lst = []
+    while n > 0:
+        lst += [n % cc]
+        n //= cc
+    return ''.join([str(x) for x in lst[::-1]])
 
 for i in range(len(a) - 2):
-    if any(a[i + z] % 12 == 0 for z in [0, 1, 2]) and all(a[i + z] % 3 == 0 for z in [0, 1, 2]):
-        ans.append(int((a[i] + a[i + 1] + a[i + 2]) / 3))
+    if any(f(a[i + g], 3)[-1] == '2' for g in [0,1,2]):
+        ans.append(min(a[i], a[i + 1], a[i + 2]))
 
-print(len(ans), min(ans))
+print(len(ans), sum(ans))
