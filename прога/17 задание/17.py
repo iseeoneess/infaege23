0598e81041a86ff71dd01d50c8582d6eaeedab2f
79533 <iseeone@mail.ru>
2023-06-15 21:08:01 +0300

    2238(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,9 +1,15 @@
-a = [int(x) for x in open('17_2401.txt')]
+a = [int(x) for x in open('17_2238.txt')]
+
+sr_ALL = sum(a) / len(a)
 
 ans = []
 
-for i in range(len(a) - 1):
-    if 50 <= sum(abs(x) for x in [a[i], a[i + 1]]) <= 200:
-        ans.append([a[i], a[i + 1]])
+for i in range(len(a) - 2):
+    d = [a[i], a[i + 1], a[i + 2]]
+    if all(z > sr_ALL for z in d) \
+            or all(z > sr_ALL for z in [a[i], a[i + 1]]) \
+            or all(z > sr_ALL for z in [a[i], a[i + 2]]) \
+            or all(z > sr_ALL for z in [a[i + 1], a[i + 2]]):
+        ans.append(sum(d))
 
-print(len(ans), ans)
+print(len(ans), max(ans))
