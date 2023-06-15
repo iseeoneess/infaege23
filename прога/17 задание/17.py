ce28200d29692f1b0321e92c18fae6c41ae2528f
79533 <iseeone@mail.ru>
2023-06-15 20:46:08 +0300

    2400(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,9 +1,9 @@
-a = [int(x) for x in open('17_2002.txt')]
+a = [int(x) for x in open('17_2400.txt')]
 
 ans = []
-for i in range(len(a) - 3):
-    d = [a[i], a[i + 1], a[i + 2], a[i + 3]]
-    if len(d) == len(set(d)) and max(d) - min(d) > 1000 and a[i] > a[i + 1] > a[i + 2] > a[i + 3]:
-        ans.append(a[i] + a[i + 1] + a[i + 2] + a[i + 3])
 
-print(len(ans), min(ans))
+for i in range(len(a) - 1):
+    if any(a[i + g] < 0 for g in [0, 1]) and sum(x for x in [a[i], a[i + 1]]) >= 100:
+        ans.append(a[i] * a[i + 1])
+
+print(len(ans), max(ans))
