09ecc8f96bbe5e95b71f0231a5a197249b3bebff
79533 <iseeone@mail.ru>
2023-06-15 20:51:53 +0300

    2401(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,9 +1,9 @@
-a = [int(x) for x in open('17_2400.txt')]
+a = [int(x) for x in open('17_2401.txt')]
 
 ans = []
 
 for i in range(len(a) - 1):
-    if any(a[i + g] < 0 for g in [0, 1]) and sum(x for x in [a[i], a[i + 1]]) >= 100:
-        ans.append(a[i] * a[i + 1])
+    if 50 <= sum(abs(x) for x in [a[i], a[i + 1]]) <= 200:
+        ans.append([a[i], a[i + 1]])
 
-print(len(ans), max(ans))
+print(len(ans), ans)
