68696438399f230d9efb544fe3d7523e5d979499
79533 <iseeone@mail.ru>
2023-06-15 19:30:38 +0300

    1998(17)

--- прога/17 задание/17.py
+++ прога/17 задание/17.py
@@ -1,9 +1,9 @@
-a = [int(x) for x in open('17_1998.txt')]
+a = [int(x) for x in open('17_1999.txt')]
 
 ans = []
 
 for i in range(len(a) - 2):
-    if (a[i] * a[i + 1] * a[i + 2]) % 7 == 0 and str(a[i] + a[i + 1] + a[i + 2])[-1] == '5':
-        ans.append(a[i] + a[i + 1] + a[i + 2])
+    if any(a[i + z] % 12 == 0 for z in [0, 1, 2]) and all(a[i + z] % 3 == 0 for z in [0, 1, 2]):
+        ans.append(int((a[i] + a[i + 1] + a[i + 2]) / 3))
 
-print(len(ans), max(ans))
+print(len(ans), min(ans))
