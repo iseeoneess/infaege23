9c14fdb7aaddac9b99500839db1555e805e8cc22
79533 <iseeone@mail.ru>
2023-06-16 02:24:26 +0300

    8741(13)

--- прога/13 задание/13.py
+++ прога/13 задание/13.py
@@ -1,10 +1,7 @@
-s = 'АЖБ БЗВ ВГ ГДЖЗ ДЖАЕК ЕАИ ЖЗ ЗВА ИА КЕ'
+s = 'ABM BCM CND DGE EF GEF HNG MCNH NDG TAMHF'
 d = {c[0]:c[1:] for c in s.split()}
 
-
 def f(s, end):
-    if s[-1] == end : return 1
-    return sum(f(s + c, end) for c in d[s[-1]] if c not in s)
-
-
-print(f('А','З') + f('В','З'))
+    if s[-1] == end: return (s.count('G') == 1 and s.count('C') == 1) or (s.count('C') == 1) or (s.count('G') == 1)
+    return sum(f(s + c, end) for c in d[s[-1]])
+print(f('T','F'))
