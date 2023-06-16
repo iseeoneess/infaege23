0034e6ecaf7f224bacbf2379085501223dd71c17
79533 <iseeone@mail.ru>
2023-06-16 18:46:14 +0300

    2426(24)

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,7 +1,6 @@
-s = open('24_865.txt').readline()
+s = open('24_2426.txt').readline()
 ans = []
-s = s.replace('C', ' ').replace('F', ' ')
-
+s = s.replace('C', ' ').replace('B', ' ').replace('A', ' ')
 for word in s.split():
     mx = 0
     mx = len(word)
