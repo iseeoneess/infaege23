ece0f9fc19601606c5998364c1b5fa693a91cec1
79533 <iseeone@mail.ru>
2023-06-17 00:37:17 +0300

    1146(24) в картинке все

--- прога/24 задание/tttt.py
+++ прога/24 задание/tttt.py
@@ -1,5 +1,5 @@
-s = open('24_1145 (1).txt').readline()
-s = s.replace('D', ' ')
-print(min(len(c) for c in s.split()) + 2)
-
-
+s = open('24_1146.txt').readline()
+s = s.replace('B', 'A').replace('C', 'A').replace('E', 'A').replace('F', 'A').replace('A', ' ').split()
+i = 1
+while s.count('D'*i) == 0: i += 1
+print(i)
