1d6ed825348068eb23df7b00123279c7b2115614
79533 <iseeone@mail.ru>
2023-06-17 00:16:53 +0300

    1145(24) в картинке все

--- прога/24 задание/tttt.py
+++ прога/24 задание/tttt.py
@@ -1,12 +1,5 @@
-s = open('24_21.txt').readline()
+s = open('24_1145 (1).txt').readline()
+s = s.replace('D', ' ')
+print(min(len(c) for c in s.split()) + 2)
 
-c = m = 0
-k = 0
-for j in 0,1:
-    c = 0 
-    for i in range(j, len(s) - 1, 2):
-        if s[i] != s[i + 1]:
-            c += 1
-            m = max(m,c)
-        else: c = 0
-print(m)
+
