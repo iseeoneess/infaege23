20d9a5256f21bde122132c1afcea9b630d33c19b
79533 <iseeone@mail.ru>
2023-06-18 00:14:34 +0300

    2500(24)EZ

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,14 +1,5 @@
-s = open('24_2499.txt').readline()
-s = s.replace("Y", "Z").replace("Z", "*")
-
-while any(z in s for z in ["XXXXX", 'XXXXXX', "XXXXXXX"]):
-    s = s.replace("XXXXX", "XXXX XXXX")
-    s = s.replace("XXXXXX", "XXXX XXXX XXXX")
-    s = s.replace("XXXXXXX", "XXXX XXXX XXXX XXXX")
+s = open('24_2500.txt').readline()
 k = 0
-
 for i in range(len(s) - 3):
-    if s[i] + s[i + 1] + s[i + 2] + s[i + 3] == "XXXX":
-       k += 1
+    if s[i] + s[i + 2] + s[i + 3] == 'GME': k += 1
 print(k)
-print(s.count("XXXX"))
