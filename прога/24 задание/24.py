e41c5e8787010d29be0ac7c067da161a08df00e4
79533 <iseeone@mail.ru>
2023-06-18 00:59:19 +0300

    2503(24)

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,10 +1,13 @@
 k = 0
-for s in open('24_2502.txt'):
-    cnt = 0
-    for i in range(len(s) - 3):
-        if s[i] + s[i + 2] + s[i + 3] == 'KGE':
-            cnt += 1
-        if cnt >= 1:
-            k += 1
-            break
+for s in open("24_2503.txt"):
+    o_cnt = a_cnt = 0
+    for i in range(len(s) - 2):
+        if s[i] + s[i + 1] + s[i + 2] == 'AOA' and s[i + 3] != 'O' and s[i - 1] != 'O':
+            a_cnt += 1
+        if s[i] + s[i + 1] + s[i + 2] == 'OAO' and s[i + 3] != 'A' and s[i - 1] != 'A':
+            o_cnt += 1
+        if i == len(s) - 3:
+            if a_cnt > o_cnt:
+                k += 1
+
 print(k)
