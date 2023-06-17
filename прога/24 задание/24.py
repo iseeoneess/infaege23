8608238c26d5caa9cfa53436d806030f440ec80e
79533 <iseeone@mail.ru>
2023-06-17 20:25:51 +0300

    5171(24)

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,3 +1,15 @@
-s = open("24_4642.txt").readline()
-s = s.replace("B", "A").replace("2", "1").replace("A1", "*").replace("A", " ").replace("1", " ")
-print(max(len(c) for c in s.split()))
+s = open("24_5171.txt").readline()
+
+s = s.replace("B", "D").replace("E", "D").replace("F", "D").replace("D", "*")
+
+m = 0
+sub = ""
+for i in range(2, len(s) - 1):
+    if s[i] + s[i + 1] == "CA":
+        sub += "CA"
+        m = max(m, len(sub))
+    if s[i] == "C" and s[i - 2] + s[i - 1] == "CA":
+        sub += "C"
+        m = max(m, len(sub))
+        sub = s[i]
+print(m)
