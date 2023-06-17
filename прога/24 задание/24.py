5e483f292c0323b81957d5fabc6a6d609f6fb1c7
79533 <iseeone@mail.ru>
2023-06-18 00:16:34 +0300

    2501(24)EZ

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,5 +1,5 @@
-s = open('24_2500.txt').readline()
+s = open('24_2501.txt').readline()
 k = 0
-for i in range(len(s) - 3):
-    if s[i] + s[i + 2] + s[i + 3] == 'GME': k += 1
+for i in range(len(s) - 5):
+    if s[i] + s[i + 2] + s[i + 4]  == 'AAA': k += 1
 print(k)
