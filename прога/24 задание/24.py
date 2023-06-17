14b4f7df47fa495fafabac073c6bf670c1b4478d
79533 <iseeone@mail.ru>
2023-06-17 19:51:31 +0300

    4642(24)

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,10 +1,3 @@
-s = open('24_2251 (1).txt').readline()
-
-s = s.split("D")
-
-m = 0
-
-for i in range(len(s) - 2):
-    sub = s[i] + "D" + s[i + 1] + 'D' + s[i + 2] + 'D'
-    m = max(m, len(sub))
-print(m)
+s = open("24_4642.txt").readline()
+s = s.replace("B", "A").replace("2", "1").replace("A1", "*").replace("A", " ").replace("1", " ")
+print(max(len(c) for c in s.split()))
