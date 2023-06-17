bc323b077bd42d0a9a981fd0f9b76d1b4c292e68
79533 <iseeone@mail.ru>
2023-06-18 00:12:42 +0300

    2499(24) DOUBLE TEST

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,6 +1,14 @@
-s = open('24_2498.txt').readline()
-while "XIXIX" in s:
-    s = s.replace('XIXIX', "XIX XIX")
+s = open('24_2499.txt').readline()
+s = s.replace("Y", "Z").replace("Z", "*")
 
-s = s.replace("XIX", "*")
-print(s.count('*'))
+while any(z in s for z in ["XXXXX", 'XXXXXX', "XXXXXXX"]):
+    s = s.replace("XXXXX", "XXXX XXXX")
+    s = s.replace("XXXXXX", "XXXX XXXX XXXX")
+    s = s.replace("XXXXXXX", "XXXX XXXX XXXX XXXX")
+k = 0
+
+for i in range(len(s) - 3):
+    if s[i] + s[i + 1] + s[i + 2] + s[i + 3] == "XXXX":
+       k += 1
+print(k)
+print(s.count("XXXX"))
