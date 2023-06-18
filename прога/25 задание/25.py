a94b32c9a213dcd2c25709f89687c0deb4df4738
79533 <iseeone@mail.ru>
2023-06-18 16:55:00 +0300

    2595(25)

--- прога/25 задание/25.py
+++ прога/25 задание/25.py
@@ -1,3 +1,6 @@
+from math import prod
+
+
 def div(x):
     d = set()
     for i in range(2, int(x ** 0.5) + 1):
@@ -6,12 +9,11 @@ def div(x):
 
 
 k = 0
-
-for x in range(550_001, 10 ** 10):
+for x in range(400_000_001, 10 ** 20):
     d = div(x)
-    if len(d) > 0:
-        f = sum(x for x in d) // len(d)
-        if f % 31 == 13:
+    if len(d) >= 5:
+        PN = d[0] * d[1] * d[2] * d[3] * d[4]
+        if PN <= x and str(PN)[-2:] == '17':
             k += 1
-            print(x, f)
+            print(PN, d[4])
     if k == 5: break
