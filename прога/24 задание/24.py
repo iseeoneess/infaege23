42b1be5fa59737d9eddebdb0a27851d244d9ffb7
79533 <iseeone@mail.ru>
2023-06-18 13:24:17 +0300

    2509(24)

--- прога/24 задание/24.py
+++ прога/24 задание/24.py
@@ -1,4 +1,8 @@
-s = open('24_2506.txt').readline().strip()
+s = open('24_2509.txt').readline().strip()
+
 d = {x:s.count(x) for x in sorted(set(s))}
-m = max(d.values())
-print([x for x in d if d[x] == m], m)
+mx = max(d.values())
+mn = min(d.values())
+
+print([x for x in d if d[x] == mx], [x for x in d if d[x] == mn])
+print(mx - mn)
