0c1dd5486eade2fdf76f7f18502ff794218fc3d0
79533 <iseeone@mail.ru>
2023-06-18 17:30:28 +0300

    953(25)

--- прога/25 задание/25.py
+++ прога/25 задание/25.py
@@ -1,12 +1,16 @@
-def p(x): return x > 1 and all(x % i != 0 for i in range(2, int(x ** 0.5) + 1))
+def p(x): return x > 1 and all(x % i != 0 for i in range(2, int(x**0.5) + 1))
 
 def div(x):
     d = set()
-    for i in range(2, int(x**0.5) + 1):
+    for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0: d |= {i, x // i}
     return sorted(d)
-
-for x in range(25_317, 51_237 + 1):
+k = 0
+for x in range(499_999, 2, - 1):
     d = div(x)
-    chk_D = [z for z in d if p(z)]
-    if len(chk_D) >= 6: print(x, max(chk_D))
+    if len(d) > 0:
+        s = sum(z for z in d if p(z))
+        if s != 0 and s % 10 == 0:
+            k += 1
+            print(x, s)
+    if k == 7: break
