499b94572ede6a97f3f4721152ae55316cff617d
79533 <iseeone@mail.ru>
2023-06-18 17:41:10 +0300

    2591(25)

--- прога/25 задание/25.py
+++ прога/25 задание/25.py
@@ -1,16 +1,17 @@
-def p(x): return x > 1 and all(x % i != 0 for i in range(2, int(x**0.5) + 1))
+def p(x): return x > 1 and all(x % i != 0 for i in range(2, int(x ** 0.5) + 1))
+
 
 def div(x):
     d = set()
-    for i in range(2, int(x ** 0.5) + 1):
+    for i in range(1, int(x ** 0.5) + 1):
         if x % i == 0: d |= {i, x // i}
     return sorted(d)
-k = 0
-for x in range(499_999, 2, - 1):
+
+for x in range(125_697, 125_721 + 1):
     d = div(x)
     if len(d) > 0:
-        s = sum(z for z in d if p(z))
-        if s != 0 and s % 10 == 0:
-            k += 1
-            print(x, s)
-    if k == 7: break
+        df_d = [z for z in d if p(z)]
+        if len(df_d) == 2:
+            prd = df_d[0]*df_d[1]
+            if x == prd: print(df_d[0], df_d[1])
+
