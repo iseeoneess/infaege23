85fc313498ea0e5d58eb50d829ccc59e7d4bd1ce
79533 <iseeone@mail.ru>
2023-06-18 17:11:36 +0300

    1078(25)

--- прога/25 задание/25.py
+++ прога/25 задание/25.py
@@ -1,17 +1,12 @@
 def div(x):
     d = set()
-    for i in range(1, int(x ** 0.5) + 1):
+    for i in range(2, int(x**0.5) + 1):
         if x % i == 0: d |= {i, x // i}
     return sorted(d)
 
-
-k = 0
-
-for x in range(190_201, 190260 + 1):
+for x in range(1_204_300, 1_204_380 + 1):
     d = div(x)
     if len(d) > 0:
-        p = [x for x in d if x % 2 == 0]
-        if len(p) == 4:
-            k += 1
-            print(p[-1], p[-2])
-    if k == 5: break
+        s = sum(z for z in d if z % 2 == 0)
+        if s % 10 == 0 and s != 0:
+            print(x, s)
