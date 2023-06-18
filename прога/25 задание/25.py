b76b11c4fcdfbc4e311c49967b83e193c20c8019
79533 <iseeone@mail.ru>
2023-06-18 17:20:12 +0300

    2590(25)

--- прога/25 задание/25.py
+++ прога/25 задание/25.py
@@ -1,16 +1,5 @@
-def div(x):
-    d = set()
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0: d |= {i, x // i}
-    return sorted(d)
+def p(x):
+    return x > 1 and all(x % i != 0 for i in range(2, int(x**0.5) + 1))
 
-
-k = 0
-for x in range(300_001, 10 ** 10):
-    d = div(x)
-    if len(d) > 0:
-        p = [z for z in d if z % 3 == 0 and z != x]
-        if len(p) == 5:
-            k += 1
-            print(x, max(p))
-    if k == 4: break
+for x in range(6_080_068, 6_080_176 + 1):
+    if p(x): print(x)
