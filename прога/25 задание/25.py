cbbe66dff44a0c77b7c9c81419df267f56395456
79533 <iseeone@mail.ru>
2023-06-18 20:07:00 +0300

    4902(25) maska + del

--- прога/25 задание/25.py
+++ прога/25 задание/25.py
@@ -1,19 +1,12 @@
 from fnmatch import *
-
-
 def div(x):
     d = set()
-    for i in range(1, int(x ** 0.5) + 1):
+    for i in range(1, int(x**0.5) + 1):
         if x % i == 0: d |= {i, x // i}
     return sorted(d)
 
-
-k = 0
-for x in range(0, 10 ** 10):
-    if fnmatch(str(x), '?6*6*?6'):
-        if x % 6 == 0 and x % 7 == 0 and x % 8 == 0:
-            d = div(x)
-            sm_d = sum(d)
-            print(x, sm_d)
-            k += 1
-    if k == 7: break
+for x in range(0, 10**7, 217):
+    if fnmatch(str(x), '14?4*'):
+        d = div(x)
+        nechet_d = [z for z in d if z % 2 != 0]
+        print(x, sum(nechet_d))
