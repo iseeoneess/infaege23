e434664d95da6f6017eeeb47b7e82cfa4825dc05
79533 <iseeone@mail.ru>
2023-06-18 19:48:30 +0300

    4899(25) maska + delit

--- прога/25 задание/25.py
+++ прога/25 задание/25.py
@@ -1,5 +1,17 @@
 from fnmatch import *
+def div(x):
+    d = set()
+    for i in range(1, int(x**0.5) + 1):
+        if x % i == 0: d |= {i, x // i}
+    return sorted(d)
+k = 0
+for x in range(65_001, 10**10):
+    if fnmatch(str(x), '6*97*5?'):
+        d = div(x)
+        if len(d) > 0:
+            check_chet = [z for z in d if z % 2 == 0]
+            if len(check_chet) >= 4:
+                k += 1
+                print(x, sum(check_chet))
 
-for x in range(0, 10**10, 2023):
-    if fnmatch(str(x), '1?2139*4'):
-        print(x, x // 2023)
+    if k == 7: break
